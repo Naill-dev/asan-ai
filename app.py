1e8e0a150c560fa91b0830ba382b87b0841ad19c
Nail <mnail6958@gmail.com>
2026-02-25 16:11:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -193,5 +193,5 @@ def init_db():
 
 if __name__ == '__main__':
      with app.app_context():
-    init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+       init_db()
+       app.run(debug=True, host='0.0.0.0', port=5000)
