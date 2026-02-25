fd83c64d7fb4eb55b59a99121f39b25247583653
Nail <mnail6958@gmail.com>
2026-02-25 16:14:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -193,5 +193,11 @@ def init_db():
 
 if __name__ == '__main__':
      with app.app_context():
-       init_db()
+        db.create_all()
+        # Default admin yarat
+        if not Admin.query.filter_by(username='admin').first():
+            admin = Admin(username='admin')
+            admin.set_password('admin123')
+            db.session.add(admin)
+            db.session.commit()
        app.run(debug=True, host='0.0.0.0', port=5000)
